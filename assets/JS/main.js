6d28771de89fe5c6f8d75b44c8357bc200e9abc5
Pete Widders <pwidders@hotmail.com>
2019-01-21 22:26:01 -0800

    Links, styling and animation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,19 @@
+$(document).ready(function() {
+  // all custom jQuery will go here
+  console.log("I hear you");
 
-function myFunction() {
+  // anime({
+  //   targets: 'a.nav-link',
+  //   translateX: [
+  //     { value: 50, duration: 2000, delay: 300 },
+  //     //{ value: 0, duration: 350 }
+  //   ]
+  // })
+  
+  $('#avatar').hide().fadeIn(5000);
+  
+
+  function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
@@ -7,3 +21,5 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+})
+
